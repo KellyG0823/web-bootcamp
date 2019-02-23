3db024d3bda788cf6cfabb0d2da443ad97795761
Kelly Graustein <graustke@gmail.com>
2019-02-23 14:44:14 -0800

    new javascript practice

---
+++ javascript-bootcamp/js-exercise.js
@@ -0,0 +1,8 @@
+// var firstName = prompt("What is your first name?");
+// var lastName = prompt("What is your last name?");
+// var age = prompt("What is your age?");
+// console.log("Your full name is " + firstName + " " + lastName + ".");
+// console.log("Your age is " + age + " years old.");
+
+var yearsOld = prompt("How old are you?");
+alert("You are " + yearsOld * 365 + " days old.");
---
+++ javascript-bootcamp/practice.js
@@ -0,0 +1,4 @@
+console.log("Mya is the best!");
+var userName = prompt("What is your name?");
+alert("Nice to meet you, " + userName);
+console.log("Also great to meet you, " + userName);
