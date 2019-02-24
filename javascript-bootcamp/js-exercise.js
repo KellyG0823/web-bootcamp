eb28f1b2c12af5704b587223ea9305edd7373bf1
Kelly Graustein <graustke@gmail.com>
2019-02-23 19:29:31 -0800

    cleaned folder

--- javascript-bootcamp/js-exercise.js
+++ javascript-bootcamp/js-exercise.js
@@ -5,4 +5,13 @@
 // console.log("Your age is " + age + " years old.");
 
 var yearsOld = prompt("How old are you?");
-alert("You are " + yearsOld * 365 + " days old.");
+// alert("You are " + yearsOld * 365 + " days old.");
+if (Math.pow(yearsOld, 0.5) % 1 === 0) {
+    console.log("Your age is a perfect square!!!");
+} else if (yearsOld === "21") {
+    console.log("Happy 21st Birthday! Here's a free shot!");
+} else if (yearsOld % 2 === 1) {
+    console.log("Your age is odd!");
+} else if(yearsOld < 0) {
+    console.log("Error!!!!");
+}
