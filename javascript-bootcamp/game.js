f0a44368320ac0e3127843754a8dc8c6b90e27c7
Kelly Graustein <graustke@gmail.com>
2019-02-24 16:58:56 -0800

    created new javascript practice files

---
+++ javascript-bootcamp/game.js
@@ -0,0 +1,29 @@
+// var secretNumber = 4;
+
+
+
+// do {
+//     var guess = Number(prompt("Guess a number"));
+//     if (guess === secretNumber) {
+//         alert("You got it right!!");
+//     } else if (guess > secretNumber) {
+//         alert("Too high, guess again");
+//     } else if (guess < secretNumber) {
+//         alert("Too low, guess again!");
+//     } else {
+//         alert("You got it wrong!");
+//     }
+// } 
+//     while (guess !== secretNumber);
+for(var count = 0; count < 6; count++){
+    console.log(count);
+}
+for(var i = -10; i<= 19; i++){
+    console.log(i);
+}
+console.log("odd numbers");
+for(var i = 300; i <= 333; i ++){
+    if(i % 2 !== 0) {
+        console.log(i);
+    }
+}
